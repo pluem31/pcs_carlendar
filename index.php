4ae147e61aa425dd71069adf15d45f6013e42df0
Piyathida Suwankit <pleum7749.catcat@gmail.com>
2021-08-31 17:00:34 +0700

    save3

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $order = 1;
 
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">ระบบบันทึกรายละเอียดการทำงานของพนักงาน Test</a>
+  <a class="navbar-brand" href="#">ระบบบันทึกรายละเอียดการทำงานของพนักงาน</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
